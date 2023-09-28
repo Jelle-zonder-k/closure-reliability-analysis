390bc48abf95732a0613cc5b63fc780f6ef077c8
jelle <j.epema@fleetcleaner.com>
2023-09-28 22:34:53 +0200

    Update repo

--- reliability/database/session_factory.py
+++
@@ -1,6 +0,0 @@
-# database/session_factory.py
-from sqlalchemy.orm import sessionmaker
-# Adjust the import based on your folder structure
-from .engine_config import engine
-
-SessionFactory = sessionmaker(bind=engine)
